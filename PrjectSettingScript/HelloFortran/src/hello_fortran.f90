1fb96942f74991e53b81c889e41879868336b77c
kimmo <hemakim0215@naver.com>
2025-09-07 12:01:14 +0900

    .

--- PrjectSettingScript/HelloFortran/src/hello_fortran.f90
+++ PrjectSettingScript/HelloFortran/src/hello_fortran.f90
@@ -8,4 +8,19 @@ program hello_fortran
   character :: char_var
   logical :: logical_var
   print *, "Hello, Fortran from HelloFortran!"
+
+
+  integer_var = 42
+  integer_var_2 = 7
+  real_var = 3.14
+  complex_var = (2.0, 3.0)
+  char_var = 'A'
+  logical_var = .true.
+  print *, "Integer variable 1:", integer_var
+  print *, "Integer variable 2:", integer_var_2
+  print *, "Real variable:", real_var
+  print *, "Complex variable:", complex_var
+  print *, "Character variable:", char_var
+  print *, "Logical variable:", logical_var
+
 end program hello_fortran
